8743f3734d9148b07edb1b5e396aac501aa7897e
Oleksii Borysonok <alex.borysonok@gmail.com>
2017-09-27 17:11:43 -0400

    index temlate and nunjucks all renders properly

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
 const chalk = require("chalk");
+const morgan = require("morgan");
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const app = express();
+
+////////////////////////// NUNJUCKS ////////////////////////////////
+
+const locals = {
+  title: "An Example",
+  secondTitle: "Really, it is a very cool Example",
+  people: [{ name: "Gandalf" }, { name: "Frodo" }, { name: "Hermione" }],
+  things: [
+    { property: "Car" },
+    { property: "Boat" },
+    { property: "Plane" },
+    { property: "Train" }
+  ]
+};
+
+const people = [{ name: "Full" }, { name: "Stacker" }, { name: "Son" }];
+
+app.set("view engine", "html");
+app.engine("html", nunjucks.render);
+nunjucks.configure("views", { noCache: true });
 
 /////////////////////////// USE /////////////////////////////////////
 
 app.use("/special/", function(req, res, next) {
   console.log("\nYou reached the special area.");
   //res.send(req.url);
-  res.send('hello everybody in SPECIAL section');
+  res.send("hello everybody in SPECIAL section");
   next();
 });
 
 app.use(function(req, res, next) {
-  console.log('\nGET ', req.url);
+  console.log("\nGET ", req.url);
   //console.log(chalk.red("Time: %d", Date.now()));
   console.log(req.method, req.path, res.statusCode);
+
+  nunjucks.render("index.html", locals, function(err, output) {
+    console.log(output);
+  });
   next();
 });
 
 //////////////////////////// GET ////////////////////////////////////
 
 app.get("/", function(req, res) {
-  res.send("Welcome to my MAIN webpage...");
+  //res.send("Welcome to my MAIN webpage...");
+  res.render("index", locals);
+  //res.render("index", { title: "Hall of Fame", people: people });
 });
 
 app.get("/news", function(req, res) {
   res.send("This is a NEWS page...");
 });
 
+////////////////////////// LISTEN //////////////////////////////////
+
 app.listen(3000, function() {
-  //console.log('server is listening...');
   process.stdout.write(chalk.blue("Server is listening..."));
 });
