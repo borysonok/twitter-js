21b6b0c9fbd8af7d78a4fb3d394700cbc8e51a4d
Oleksii Borysonok <alex.borysonok@gmail.com>
2017-09-27 12:16:01 -0400

    logging middleware (app.use) step

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-const chalk = require('chalk');
+const chalk = require("chalk");
 const express = require("express");
 const app = express();
 
+/////////////////////////// USE /////////////////////////////////////
 
-
-
-
-app.get('/', function(req, res){
-    res.send('Welcome to my MAIN webpage...');
+app.use("/special/", function(req, res, next) {
+  console.log("\nYou reached the special area.");
+  //res.send(req.url);
+  res.send('hello everybody in SPECIAL section');
+  next();
 });
 
-app.get('/news', function(req, res){
-    res.send('This is a NEWS page...');
+app.use(function(req, res, next) {
+  console.log('\nGET ', req.url);
+  //console.log(chalk.red("Time: %d", Date.now()));
+  console.log(req.method, req.path, res.statusCode);
+  next();
 });
 
+//////////////////////////// GET ////////////////////////////////////
 
-
-app.listen(3000, function(){
-    //console.log('server is listening...');
-    process.stdout.write(chalk.blue('Server is listening...'));
+app.get("/", function(req, res) {
+  res.send("Welcome to my MAIN webpage...");
 });
 
+app.get("/news", function(req, res) {
+  res.send("This is a NEWS page...");
+});
 
+app.listen(3000, function() {
+  //console.log('server is listening...');
+  process.stdout.write(chalk.blue("Server is listening..."));
+});
